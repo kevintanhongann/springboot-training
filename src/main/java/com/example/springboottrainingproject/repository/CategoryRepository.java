d3eb95a677429c13d77f6a75080121376b6da127
kevintan <kevintanhongann@gmail.com>
2017-11-22 20:55:20 +0800

    replace controller implementation by using repositoryrestresource

--- src/main/java/com/example/springboottrainingproject/repository/CategoryRepository.java
+++ src/main/java/com/example/springboottrainingproject/repository/CategoryRepository.java
@@ -2,8 +2,9 @@ package com.example.springboottrainingproject.repository;
 
 import com.example.springboottrainingproject.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+@RepositoryRestResource(path = "/category", collectionResourceRel = "categories")
+public interface CategoryRepository extends PagingAndSortingRepository<Category, Long> {
 }
--- src/main/java/com/example/springboottrainingproject/repository/JournalRepository.java
+++ src/main/java/com/example/springboottrainingproject/repository/JournalRepository.java
@@ -2,9 +2,11 @@ package com.example.springboottrainingproject.repository;
 
 import com.example.springboottrainingproject.domain.Journal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource
-public interface JournalRepository extends JpaRepository<Journal, Long>{
-  
+@RepositoryRestResource(path = "/entry", collectionResourceRel = "entries")
+public interface JournalRepository extends PagingAndSortingRepository<Journal, Long> {
+
 }
